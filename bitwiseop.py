04bfce3a9c954d9c44e57eec8d60f6755638b62d
sethdahanayake-tech <seth.dahanayake@gmail.com>
2026-06-28 11:23:47 +1200

    Add files via upload

---
+++ bitwiseop.py
@@ -0,0 +1,19 @@
+#Write a program to apply the right shift and left shift bitwise operator.
+a = 10
+
+b = -10
+
+# print bitwise right shift operator
+
+print("a >> 1 =", a >> 1)
+
+print ("b >> 1 =", b >> 1)
+
+
+# print bitwise left shift operator
+
+print("a << 1 =", a << 1)
+
+print ("b << 1 =", b << 1)
+
+
